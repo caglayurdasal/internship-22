2291896856c228a29c60aeb818e9218ef5e3e505
caglayurdasal <caglayurdasal@gmail.com>
2022-08-11 08:49:17 +0300

    Fix bugs

--- week1/IntegerToRoman.java
+++ week1/IntegerToRoman.java
@@ -4,23 +4,12 @@ public class IntegerToRoman {
     public static String intToRoman(int num) {
         int[] numsInt = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
         String[] numsRoman = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
-        String newNum="";
-        List<Integer> intParts = new ArrayList<Integer>();
-        int remainder, numDigits = 0;
-        while (num > 0) {
-            remainder = num % 10;
-            intParts.add((int) (remainder * Math.pow(10, numDigits)));
-            numDigits++;
-            num /= 10;
-        }
-        System.out.println(intParts);
-        for (int i = intParts.size() - 1; i >= 0; i--) {
-            if(intParts.get(i)!=0){
-                for(int j=0;j<numsInt.length;j++){
-                    if (numsInt[j]==intParts.get(i)){
-                        newNum+=numsRoman[j];
-                    }
-                }
+        String newNum = "";
+
+        for(int i=numsInt.length-1;i>=0;i--){
+            while(num > numsInt[i]){
+                num-=numsInt[i];
+                newNum += numsRoman[i];
             }
         }
         return newNum;
