ee4aeebe5e779edd7301b1683a90f94c006e7bc4
caglayurdasal <caglayurdasal@gmail.com>
2022-08-11 10:43:12 +0300

    Add readDatabase and database.txt

---
+++ week1/readDatabase.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class readDatabase {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static boolean isInFile(String userMail) {
+        try (FileReader database = new FileReader("database.txt");
+             BufferedReader br = new BufferedReader(database)) {
+            String mail = br.readLine();
+            while (mail != null) {
+                if (mail.contains(userMail)) {
+                    System.out.println("Welcome, " + userMail);
+                    return true;
+                }
+                mail = br.readLine();
+            }
+            return false;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter your e-mail address: ");
+        String mailAddress = scanner.nextLine();
+        while (!isInFile(mailAddress)) {
+            System.out.println("You have entered an invalid e-mail address. Please try again.");
+            mailAddress = scanner.nextLine();
+        }
+
+    }
+}
